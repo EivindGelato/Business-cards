7a03f9ca85ead818d1f5744f6621aa32f4e1c509
Eivind Ingebrigtsen <eivind@gelato.com>
2020-06-25 23:14:47 +0200

    Latest

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const printApi = require("print-api")
-const print = printApi('0E4OaqeFBW3djipN01sLY9YMDN433hUP1WwPxUmF', { test: false });
+const print = printApi('open-api-gelato-live-cc5976f6-f003-4b72-8fa1-7ec6b5f974e8:63d442a9-2ad7-430f-bbe8-fca9f91ae1cc', { test: false });
 let example = print.example();
 
 var count = require('./count.json');
@@ -14,29 +14,49 @@ require('node-json-color-stringify');
 // Get a print quote
 var order = [{ 
 order: 
-   { orderReferenceId: parseFloat(1456+''+count.count),
-     customerReferenceId: parseFloat(1556+''+count.count),
+   { orderReferenceId: parseFloat(109887+''+count.count),
+     customerReferenceId: parseFloat(109887+''+count.count),
      currencyIsoCode: 'USD' 
  },
-  recipient: {
-        "countryIsoCode": "SE",
-        "addressLine1": "Snarøyveien 30C",
-        "addressLine2": "Varemottak Bygg J",
+ "recipient": {
+        "countryIsoCode": "NO",
+        "addressLine1": "Snarøyveien 30",
+        "addressLine2": "Varemottak - c/o Gelato",
         "city": "Fornebu",
         "postcode": "1360",
-        "firstName": "Frecipient",
-        "lastName": "Receiverson",
-        "email": "email@company.com",
-        "phone": "+15558901321"
+        "stateCode": "",
+        "firstName": "Julie",
+        "lastName": "Ryland",
+        "email": "julie.ryland@gelato.com",
+        "phone": "+4748111975"
     },
-  product: 
-   { itemReferenceId: parseFloat(1656+''+count.count),
-     productUid: "cards_pf_bc_pt_350-gsm-coated-silk_cl_4-0_ct_glossy-lamination_ver_variable",
-     pdfUrl: "https://www.dropbox.com/s/4851ph8r4xkz6x2/Variable-BusinessCard.pdf?dl=1",
-     quantity: 5 } }]
+  products: 
+   [{ 
+       itemReferenceId: parseFloat(109887+''+count.count),
+       "productUid": "cards_pf_bc_pt_350-gsm-coated-silk_cl_4-4_ct_glossy-lamination_ver_variable",
+       //"productUid": "cards_pf_bc_pt_130-lb-cover-coated-silk_cl_4-4_ct_glossy-protection_ver_variable",
+       pdfUrl: "https://www.dropbox.com/s/putl06vaxd4j1n7/B-C-Julie.pdf?dl=1",
+       quantity: 278 
+     }] 
+ }]
+
+ /*
+
+Sarah-Ann Ferguson,Network Strategy Manager,+44 7784777947,sarah-ann.ferguson@gelato.com
+ 
+
+Saket Somani
+Gelato Print Services India Pvt Ltd
+703, Tower A, 7th Floor
+Marathon Futurex,
+N M Joshi Marg
+Lower Parel (East)
+Mumbai 400 013
+Contact: +91 98195 43818
+ */
 
 
-fs.writeFileSync(path.join(__dirname, '/orders/order-'+1456+''+count.count+'.json'), JSON.stringify(order[0], null, 4))
+fs.writeFileSync(path.join(__dirname, '/orders/order-'+109887+''+count.count+'.json'), JSON.stringify(order[0], null, 4))
 
 console.log('==========================================================')
 console.log('EXAMPLE REQUEST')
@@ -48,28 +68,26 @@ console.log('==========================================================')
 console.log('QUOTE RESPONSE WITH PROMISES')
     console.log(JSON.colorStringify(a.body, null, 2));
 
-    fs.writeFileSync(path.join(__dirname, '/orders/promise-'+1456+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
+    fs.writeFileSync(path.join(__dirname, '/orders/promise-'+109887+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
 
   
-  
-   print.order(a.body.production.shipments[0].promiseUid).then((a)=>{
-      fs.writeFileSync(path.join(__dirname, '/orders/response-'+1456+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
-      count.count++;
-      fs.writeFileSync(path.join(__dirname, 'count.json'), JSON.stringify(count, null, 4))
-      console.log('==========================================================')
-      console.log('ORDER PLACED')
+   console.log('a.body.production.shipments[3].promiseUid', a.body.production.shipments[3].promiseUid);
+    print.order(a.body.production.shipments[3].promiseUid).then((a)=>{
+       fs.writeFileSync(path.join(__dirname, '/orders/response-'+109887+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
+       count.count++;
+       fs.writeFileSync(path.join(__dirname, 'count.json'), JSON.stringify(count, null, 4))
+       console.log('==========================================================')
+       console.log('ORDER PLACED')
 
-      console.log(JSON.colorStringify(a.body, null, 2));
-   }).catch(console.error) 
+       console.log(JSON.colorStringify(a.body, null, 2));
+    }).catch(console.error) 
 
 
 }).catch(console.error)
 
-
-
 setInterval(()=>{
-  print.status('1456'+''+(count.count-1)).then((a)=>{
-    fs.writeFileSync(path.join(__dirname, '/orders/status-'+8411+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
+  print.status('109887'+''+(count.count-1)).then((a)=>{
+    fs.writeFileSync(path.join(__dirname, '/orders/status-'+109887+''+count.count+'.json'), JSON.stringify(a.body, null, 4))
     console.log('==========================================================')
     console.log('ORDER STATUS')
 
